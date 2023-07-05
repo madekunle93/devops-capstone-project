b5c8c8ed150ec7f98b886abfcbf5fd28e2856426
madekunle93 <madekunle93@outlook.com>
2023-07-05 12:19:46 -0400

    fixed whitespace

--- service/routes.py
+++ service/routes.py
@@ -9,7 +9,6 @@ from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
-
 ############################################################
 # Health Endpoint
 ############################################################
@@ -17,7 +16,6 @@ from . import app  # Import Flask application
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
-
 
 ######################################################################
 # GET INDEX
@@ -30,7 +28,6 @@ def index():
         version="1.0",
         # paths=url_for("list_accounts", _external=True),
     ), status.HTTP_200_OK
-
 
 ######################################################################
 # CREATE A NEW ACCOUNT
@@ -52,7 +49,6 @@ def create_accounts():
     location_url = "/"  # Remove once get_accounts has been implemented
     return make_response(jsonify(message), status.HTTP_201_CREATED, {"Location": location_url})
 
-
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -71,7 +67,6 @@ def list_accounts():
     
     return jsonify(serialized_accounts), status.HTTP_200_OK
 
-
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -87,7 +82,6 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
-
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -110,7 +104,6 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -128,7 +121,6 @@ def delete_accounts(account_id):
         account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
-
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
